84d87ef3575867d29c92c199fec3c898ec5655f7
Ju Liu <liuju86@gmail.com>
2023-02-05 11:24:30 +0000

    Add mastodon link

--- src/components/seo.js
+++ src/components/seo.js
@@ -68,7 +68,9 @@ function SEO({ description, lang, meta, title }) {
           content: metaDescription,
         },
       ].concat(meta)}
-    />
+    >
+      <link rel="me" href="https://hachyderm.io/@arkham" />
+    </Helmet>
   )
 }
 
